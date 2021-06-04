79265a233b7401262af0da5a8a806cd276375e45
Joel Rdz <joelrdzh@gmail.com>
2021-06-04 13:33:53 -0500

    Basic routing with React Router and pages scaffold

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { Switch, Route } from 'react-router-dom';
+
+import ProjectsPage from './pages/Projects';
+import AboutPage from './pages/About';
+import ContactPage from './pages/Contact';
+
 function App() {
   return (
-    <div>
-      Plataforma React
-    </div>
+    <>
+      <div>Plataforma React</div>
+      <Switch>
+        <Route path='/' exact>
+          <ProjectsPage />
+        </Route>
+        <Route path='/servicios'>
+          <AboutPage />
+        </Route>
+        <Route path='/contacto'>
+          <ContactPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
